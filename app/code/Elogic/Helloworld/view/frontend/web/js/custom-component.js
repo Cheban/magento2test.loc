9357dd013db40b467f78c31fba4b77f609b712e0
Cheban <timur.cheban1@gmail.com>
2019-02-11 18:24:08 +0200

    test component

--- app/code/Elogic/Helloworld/view/frontend/web/js/custom-component.js
+++ app/code/Elogic/Helloworld/view/frontend/web/js/custom-component.js
@@ -1,4 +1,4 @@
-define(['jquery', 'uiComponent', 'ko'], function ($, Component, ko) {
+define(['jquery', 'uiComponent','mage/validation', 'ko'], function ($, Component, validation, ko) {
         'use strict';
         return Component.extend({
             defaults: {
@@ -7,6 +7,8 @@ define(['jquery', 'uiComponent', 'ko'], function ($, Component, ko) {
             initialize: function () {
                 this.customerName = ko.observableArray([]);
                 this.customerSecondName = ko.observableArray([]);
+                this.customerEmail = ko.observableArray([]);
+                this.customerEmailData = ko.observable('');
                 this.customerNameData = ko.observable('');
                 this.customerSecondNameData = ko.observable('');
                 this._super();
@@ -15,7 +17,9 @@ define(['jquery', 'uiComponent', 'ko'], function ($, Component, ko) {
             addNewCustomer: function () {
                 this.customerName.push({name:this.customerNameData()});
                 this.customerSecondName.push({secondname:this.customerSecondNameData()});
+                this.customerEmail.push({email:this.customerEmailData()});
                 this.customerNameData('');
+                this.customerEmailData('');
                 this.customerSecondNameData('');
             }
         });
